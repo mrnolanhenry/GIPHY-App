503208ab92f84137f873989ad887d1a7d4d41202
enfenry <enfenry@vt.edu>
2019-01-16 23:29:17 -0500

    some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,25 +4,26 @@ $('document').ready(function () {
     const limit = 5;
 
     let animals = [
-        "Alligator", "Baboon", "Bat", "Bear", "Butterfly", "Camel", "Cat", "Cheetah", "Chicken", "Crab", "Crocodile", "Dog", "Dolphin", "Donkey", "Eagle", "Elephant", "Fish", "Flamingo", "Fox", "Frog", "Giraffe", "Goat", "Gorilla", "Hawk", "Hedgehog", "Horse", "Jellyfish", "Kangaroo", "Koala", "Lemur", "Lion", "Llama", "Lobster", "Manatee", "Monkey", "Moose", "Mouse", "Narwhal", "Octopus", "Ostrich", "Otter", "Owl", "Parrot", "Penguin", "Pig", "Porcupine", "Rabbit", "Raccoon", "Rhinoceros", "Shark", "Sheep", "Skunk", "Snake", "Tiger", "Turtle", "Whale", "Wolf", "Zebra"
+        "Alligator", "Baboon", "Bat", "Bear", "Butterfly", "Camel", "Cat", "Cheetah", "Chicken", "Crab", "Crocodile", "Dog", "Dolphin", "Donkey", "Eagle", "Elephant", "Fish", "Flamingo", "Fox", "Frog", "Giraffe", "Goat", "Gorilla", "Hawk", "Hedgehog", "Horse", "Jellyfish", "Kangaroo", "Koala", "Lemur", "Lion", "Llama", "Manatee", "Monkey", "Moose", "Mouse", "Narwhal", "Octopus", "Ostrich", "Otter", "Owl", "Parrot", "Penguin", "Pig", "Porcupine", "Rabbit", "Raccoon", "Rhinoceros", "Shark", "Sheep", "Skunk", "Snake", "Tiger", "Turtle", "Whale", "Wolf", "Zebra"
     ]
 
     addButtonArray(animals, $('.btn-array'), 'animal-option btn btn-primary');
 
     $('.animal-option').on('click', function () {
+        $('.results').empty();
         let selectedAnimal = $(this).text();
         let ajaxCall = $.get("http://api.giphy.com/v1/gifs/search?q=" + selectedAnimal + "&api_key=" + APIkey + "&limit=" + limit);
         ajaxCall.done(function(data) { 
             // console.log("success got data", data, data.data.length); 
             data.data.forEach(function (element) {
-                console.log(element.rating,)
-                let newCard = $('<div>');
+                let newCard = $('<span>');
                 let newCardBody = $('<div>');
                 let newImage = $('<img>');
+                newImage.attr('class','animal-pic');
                 newImage.attr('src',element.images.fixed_height_still.url);
                 newImage.attr('width',element.images.fixed_height_still.width);
                 newImage.attr('height',element.images.fixed_height_still.height);
-                newCard.attr('class','card');
+                newCard.attr('class','card animal-card');
                 newCardBody.attr('class','card-body');
                 newCardBody.append('Rating: ' + element.rating);
                 newCard.append(newCardBody);
